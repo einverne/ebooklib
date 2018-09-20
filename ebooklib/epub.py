ccad13f21ac691f7466dda5cbf602bed2c5c9717
Ein Verne <einverne@gmail.com>
2018-09-20 09:39:08 +0800

    Extract cover from id
    https://github.com/aerkalov/ebooklib/issues/171
    
    Signed-off-by: Ein Verne <einverne@gmail.com>

--- ebooklib/epub.py
+++ ebooklib/epub.py
@@ -1519,7 +1519,9 @@ class EpubReader(object):
                     ei.content = self.read_file(zip_path.join(self.opf_dir, ei.get_name()))
                     ei.properties = properties
             elif media_type in IMAGE_MEDIA_TYPES:
-                if 'cover-image' in properties or 'cover.jpg' in r.get('id'):
+                # take suggestion metioned here
+                # https://github.com/aerkalov/ebooklib/issues/171
+                if 'cover-image' in properties or 'cover' in r.get('id').lower():
                     ei = EpubCover(uid=r.get('id'), file_name=unquote(r.get('href')))
 
                     ei.media_type = media_type
